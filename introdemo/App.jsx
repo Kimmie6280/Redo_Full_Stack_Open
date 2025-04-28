7a94f55b717496858854043d12166a1d1b0423d8
Kimberly Ramgopal <66437777+Kimmie6280@users.noreply.github.com>
2025-04-28 07:50:42 -0400

    Add files via upload

---
+++ introdemo/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+
+   console.log("hi there")
+   const hello = "my name is kim"
+  return (
+    <div>
+      <p>Hello world</p>
+      <p>{hello}</p>
+    </div>
+  )
+}
+
+export default App
