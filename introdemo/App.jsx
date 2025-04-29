06be834f068da8d9a7fcccabb565f5c6f477fe3a
Kimberly Ramgopal <66437777+Kimmie6280@users.noreply.github.com>
2025-04-28 22:49:49 -0400

    Update App.jsx

--- introdemo/App.jsx
+++ introdemo/App.jsx
@@ -1,15 +1,17 @@
+const Hello = () => {
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+}
+
 const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  return React.createElement(
-    'div',
-    null,
-    React.createElement(
-      'p', null, 'Hello world, it is ', now.toString()
-    ),
-    React.createElement(
-      'p', null, a, ' plus ', b, ' is ', a + b
-    )
+  return (
+    <div>
+      <h1>Greetings</h1>
+
+      <Hello />
+    </div>
   )
 }
