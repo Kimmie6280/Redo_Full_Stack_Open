da2b5658b3ddbc80fcefa584191791a106937bcc
Kimberly Ramgopal <66437777+Kimmie6280@users.noreply.github.com>
2025-05-03 01:06:11 -0400

    1.3-1.5
    So the goal is to display the name and the parts and now things are in a array. so what changed is I still have 4 components header,parts,content,total. the last 3 have changed. in part it takes name and exercise which are passed into contnent. this is where things changed so now i have to pass the index of the part and excerise or name to get it to displaty. same thing in my total where i had to pass the prop.parts[index[.exercise to acces the number to add to the others. in my app i needed to now pass parts={parts} and exercise={excerise}

--- part1/courseinfo/App.jsx
+++ part1/courseinfo/App.jsx
@@ -1,55 +1,68 @@
-//1.1 - 1.2
-
 //Header takes care of rendering the name of the course,
 const Header =(props) =>{
 
   console.log(props)
+  const course = props.course
 
   return(
-    <h1> {props.course} </h1>
+    <h1> {course} </h1>
   )
 }
 
+//Part components of which each renders the name and number of exercises of one part.
+const Part = ({name,exercise }) => {
+  console.log(name, exercise)
+
+  return (
+    <p>{name} {exercise}</p>
+  )
+}
 /*Content renders the parts and their number of exercises */ 
+
 const Content = (props) =>{
+  console.log(props)
 
   return(
-    <div>
+    <>
+      <Part name={props.parts[0].name} exercise={props.parts[0].exercises}/>
+      <Part name={props.parts[1].name} exercise={props.parts[1].exercises}/>
+      <Part name={props.parts[2].name} exercise={props.parts[2].exercises}/>
 
-    <p>{props.part1} {props.exercises1}</p> 
-    <p>{props.part2} {props.exercises2}</p> 
-    <p>{props.part3} {props.exercises3}</p>
-
-    </div>
-    
+    </>
   )
 }
-
 //and Total renders the total number of exercises.
 
 const Total = (props) =>{
+  console.log(props)
+
   return(
-    <p>Total Number of Excerises {props.exercises1 + props.exercises2+props.exercises3}</p>
+    <p>Total Number of Excerises {props.parts[0].exercises + props.parts[1].exercises+props.parts[2].exercises}</p>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+ 
   return (
     <div>
       <Header course={course}/>
-      <Content 
-      part1={part1} exercises1={exercises1} 
-      part2={part2} exercises2={exercises2} 
-      part3={part3} exercises3={exercises3}/>
-      <Total exercises1={exercises1} exercises2={exercises2}exercises3={exercises3}/>
+      <Content parts={parts} exercise={parts.exercise}/>
+      <Total parts={parts}/>
     </div>
   )
 }
