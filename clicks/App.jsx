1ec1b0d32a737fad9885e1f79ce7eec621e013fa
Kimberly Ramgopal <66437777+Kimmie6280@users.noreply.github.com>
2025-05-04 20:44:49 -0400

    Update App.jsx

--- clicks/App.jsx
+++ clicks/App.jsx
@@ -1,17 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  })
+  const handleLeftClick = () =>
+    setClicks({ ...clicks, left: clicks.left + 1 })
+  
+  const handleRightClick = () =>
+    setClicks({ ...clicks, right: clicks.right + 1 })
 
   return (
     <div>
-      {left}
-      <button onClick={() => setLeft(left + 1)}>
-        left
-      </button>
-      <button onClick={() => setRight(right + 1)}>
-        right
-      </button>
-      {right}
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
     </div>
   )
 }
+
+export default App
